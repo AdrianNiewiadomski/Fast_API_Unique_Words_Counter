82071b582641daf760959a328ec518f1627ef755
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2022-04-24 01:19:24 +0200

    The words in the requests body can now be counted.

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI
+from fastapi import Request, FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def remove_punctuation_symbols(text: str) -> str:
+    punctuation_symbols = [",", ".", ":", "?", "!"]
+
+    for symbol in punctuation_symbols:
+        text = text.replace(symbol, "")
+
+    return text
+
+
+def count_unique_words(text: str) -> int:
+    text = text.upper()
+    unique_words = set(text.split())
+    print("unique_words: ", unique_words)
+    return len(unique_words)
+
+
+def analyze_data(text: str):
+    print("text: ", text)
+    text = text.replace("\n", " ")
+    text = remove_punctuation_symbols(text)
+    number_of_unique_words = count_unique_words(text)
+    return number_of_unique_words
+
+
+@app.post("/words")
+async def root(request: Request):
+    data: dict = await request.json()
+    data["uniqueWordsCount"] = analyze_data(data["text"])
+    return data
